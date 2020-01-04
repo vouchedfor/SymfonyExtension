99099edbc4ec3a8183535f246b2a621c03745968
Vouchedfor Admin <vouchedfor-admin@users.noreply.github.com>
2020-01-04 12:03:55 +0000

    Added Symfony 5 support

--- tests/Behat/Context/TestContext.php
+++ tests/Behat/Context/TestContext.php
@@ -1,32 +1,23 @@
 <?php
-
 declare(strict_types=1);
-
 namespace Tests\Behat\Context;
-
 use Behat\Behat\Context\Context;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\PhpExecutableFinder;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Yaml\Yaml;
-
 final class TestContext implements Context
 {
     /** @var string */
     private static $workingDir;
-
     /** @var Filesystem */
     private static $filesystem;
-
     /** @var string */
     private static $phpBin;
-
     /** @var Process */
     private $process;
-
     /** @var array */
     private $variables = [];
-
     /**
      * @BeforeFeature
      */
@@ -36,7 +27,6 @@ final class TestContext implements Context
         self::$filesystem = new Filesystem();
         self::$phpBin = self::findPhpBinary();
     }
-
     /**
      * @BeforeScenario
      */
@@ -45,7 +35,6 @@ final class TestContext implements Context
         self::$filesystem->remove(self::$workingDir);
         self::$filesystem->mkdir(self::$workingDir, 0777);
     }
-
     /**
      * @AfterScenario
      */
@@ -53,7 +42,6 @@ final class TestContext implements Context
     {
         self::$filesystem->remove(self::$workingDir);
     }
-
     /**
      * @Given a standard Symfony autoloader configured
      */
@@ -61,18 +49,14 @@ final class TestContext implements Context
     {
         $this->thereIsFile('vendor/autoload.php', sprintf(<<<'CON'
 <?php
-
 declare(strict_types=1);
-
 $loader = require '%s';
 $loader->addPsr4('App\\', __DIR__ . '/../src/');
 $loader->addPsr4('App\\Tests\\', __DIR__ . '/../tests/');
-
 return $loader; 
 CON
             , __DIR__ . '/../../../vendor/autoload.php'));
     }
-
     /**
      * @Given a working Symfony application with SymfonyExtension configured
      */
@@ -86,30 +70,23 @@ default:
                 class: App\Kernel
 CON
         );
-
         $this->standardSymfonyAutoloaderConfigured();
-
         $this->thereIsFile('src/Kernel.php', <<<'CON'
 <?php
-
 namespace App;
-
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Kernel as HttpKernel;
 use Symfony\Component\Routing\RouteCollectionBuilder;
-
 class Kernel extends HttpKernel
 {
     use MicroKernelTrait;
-
     public function helloWorld(): Response
     {
         return new Response('Hello world!');
     }
-
     public function registerBundles(): iterable
     {
         return [
@@ -117,7 +94,6 @@ class Kernel extends HttpKernel
             new \FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle(),
         ];
     }
-
     protected function configureContainer(ContainerBuilder $container, LoaderInterface $loader): void
     {
         $container->loadFromExtension('framework', [
@@ -135,10 +111,8 @@ class Kernel extends HttpKernel
 }
 CON
         );
-
         $this->thereIsFile('config/services.yaml', '');
     }
-
     /**
      * @Given /^an? (server|environment) variable "([^"]++)" set to "([^"]++)"$/
      */
@@ -146,7 +120,6 @@ CON
     {
         $this->variables[$type][$name] = $value;
     }
-
     /**
      * @Given /^a YAML services file containing:$/
      */
@@ -154,7 +127,6 @@ CON
     {
         $this->thereIsFile('config/services.yaml', (string) $content);
     }
-
     /**
      * @Given /^a Behat configuration containing(?: "([^"]+)"|:)$/
      */
@@ -162,31 +134,23 @@ CON
     {
         $mainConfigFile = sprintf('%s/behat.yml', self::$workingDir);
         $newConfigFile = sprintf('%s/behat-%s.yml', self::$workingDir, md5((string) $content));
-
         self::$filesystem->dumpFile($newConfigFile, (string) $content);
-
         if (!file_exists($mainConfigFile)) {
             self::$filesystem->dumpFile($mainConfigFile, Yaml::dump(['imports' => []]));
         }
-
         $mainBehatConfiguration = Yaml::parseFile($mainConfigFile);
         $mainBehatConfiguration['imports'][] = $newConfigFile;
-
         self::$filesystem->dumpFile($mainConfigFile, Yaml::dump($mainBehatConfiguration));
     }
-
     /**
      * @Given /^a (?:.+ |)file "([^"]+)" containing(?: "([^"]+)"|:)$/
      */
     public function thereIsFile($file, $content): string
     {
         $path = self::$workingDir . '/' . $file;
-
         self::$filesystem->dumpFile($path, (string) $content);
-
         return $path;
     }
-
     /**
      * @Given /^a feature file containing(?: "([^"]+)"|:)$/
      */
@@ -194,36 +158,27 @@ CON
     {
         $this->thereIsFile(sprintf('features/%s.feature', md5(uniqid('', true))), $content);
     }
-
     /**
      * @When /^I run Behat$/
      */
     public function iRunBehat(): void
     {
         $executablePath = BEHAT_BIN_PATH;
-
         if ($this->variables !== []) {
             $content = '<?php ';
-
             foreach ($this->variables['server'] ?? [] as $name => $value) {
                 $content .= sprintf('$_SERVER["%s"] = "%s"; ', $name, $value);
             }
-
             foreach ($this->variables['environment'] ?? [] as $name => $value) {
                 $content .= sprintf('$_ENV["%s"] = "%s"; ', $name, $value);
             }
-
             $content .= sprintf('require_once("%s"); ', $executablePath);
-
             $executablePath = $this->thereIsFile('__executable.php', $content);
         }
-
-        $this->process = new Process(sprintf('%s %s --strict -vvv --no-interaction --lang=en', self::$phpBin, escapeshellarg($executablePath)));
-        $this->process->setWorkingDirectory(self::$workingDir);
+        $this->process = new Process([self::$phpBin, $executablePath, '--strict', '-vvv', '--no-interaction', '--lang=en'], self::$workingDir);
         $this->process->start();
         $this->process->wait();
     }
-
     /**
      * @Then /^it should pass$/
      */
@@ -232,12 +187,10 @@ CON
         if (0 === $this->getProcessExitCode()) {
             return;
         }
-
         throw new \DomainException(
             'Behat was expecting to pass, but failed with the following output:' . \PHP_EOL . \PHP_EOL . $this->getProcessOutput()
         );
     }
-
     /**
      * @Then /^it should pass with(?: "([^"]+)"|:)$/
      */
@@ -246,7 +199,6 @@ CON
         $this->itShouldPass();
         $this->assertOutputMatches((string) $expectedOutput);
     }
-
     /**
      * @Then /^it should fail$/
      */
@@ -255,12 +207,10 @@ CON
         if (0 !== $this->getProcessExitCode()) {
             return;
         }
-
         throw new \DomainException(
             'Behat was expecting to fail, but passed with the following output:' . \PHP_EOL . \PHP_EOL . $this->getProcessOutput()
         );
     }
-
     /**
      * @Then /^it should fail with(?: "([^"]+)"|:)$/
      */
@@ -269,7 +219,6 @@ CON
         $this->itShouldFail();
         $this->assertOutputMatches((string) $expectedOutput);
     }
-
     /**
      * @Then /^it should end with(?: "([^"]+)"|:)$/
      */
@@ -277,7 +226,6 @@ CON
     {
         $this->assertOutputMatches((string) $expectedOutput);
     }
-
     /**
      * @param string $expectedOutput
      */
@@ -285,12 +233,10 @@ CON
     {
         $pattern = '/' . preg_quote($expectedOutput, '/') . '/sm';
         $output = $this->getProcessOutput();
-
         $result = preg_match($pattern, $output);
         if (false === $result) {
             throw new \InvalidArgumentException('Invalid pattern given:' . $pattern);
         }
-
         if (0 === $result) {
             throw new \DomainException(sprintf(
                 'Pattern "%s" does not match the following output:' . \PHP_EOL . \PHP_EOL . '%s',
@@ -299,21 +245,16 @@ CON
             ));
         }
     }
-
     private function getProcessOutput(): string
     {
         $this->assertProcessIsAvailable();
-
         return $this->process->getErrorOutput() . $this->process->getOutput();
     }
-
     private function getProcessExitCode(): int
     {
         $this->assertProcessIsAvailable();
-
         return $this->process->getExitCode();
     }
-
     /**
      * @throws \BadMethodCallException
      */
@@ -323,7 +264,6 @@ CON
             throw new \BadMethodCallException('Behat proccess cannot be found. Did you run it before making assertions?');
         }
     }
-
     /**
      * @throws \RuntimeException
      */
@@ -333,7 +273,6 @@ CON
         if (false === $phpBinary) {
             throw new \RuntimeException('Unable to find the PHP executable.');
         }
-
         return $phpBinary;
     }
 }
